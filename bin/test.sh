514a05540e5a18fe1b678f14aa1db715365c787d
Chuck Morris <chuck.morris@ngc.com>
2006-08-23 21:47:39 +0000

    Added batch files and shell scripts inadvertently left out of the initial load.
    git-svn-id: https://svn.code.sf.net/p/teamengine/code/trunk@4 24c30046-25e5-4eec-8353-25991c8b1332

---
+++ bin/test.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+base=`dirname $0`
+if [ -r $base/setenv.sh ]
+then
+  . $base/setenv.sh
+fi
+cp=$base/../apps/engine/resources
+for x in $base/../apps/engine/lib/*.jar
+do
+  cp=$cp:$x
+done
+for x in $base/../components/*
+do
+  cp=$cp:$base/components/$x/resources
+  for y in $base/components/$x/lib/*.jar
+  do
+    cp=$cp:$y
+  done
+done
+java=$JAVA_HOME/bin/java
+if [ ! -x $java ]
+then
+  java=java
+fi
+$java -cp $cp $JAVA_OPTS com.occamlab.te.Test -cmd=$0 $*
+
