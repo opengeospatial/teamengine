98b390136c16b6bb8b5d5327826929c3aeb52249
Jeremy Parr-Pearson <jparrpearson@galdosinc.com>
2007-06-19 17:21:03 +0000

    Fix to DateTimeConverter to modify the incoming dateTime string to proper ISO (from XML schema syntax)
    git-svn-id: https://svn.code.sf.net/p/teamengine/code/trunk@188 24c30046-25e5-4eec-8353-25991c8b1332

--- apps/engine/src/java/com/occamlab/te/util/DateTimeConverter.java
+++ apps/engine/src/java/com/occamlab/te/util/DateTimeConverter.java
@@ -1,5 +1,8 @@
 package com.occamlab.te.util;
 
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
 import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormatter;
 import org.joda.time.format.DateTimeFormat;
@@ -24,9 +27,33 @@ public class DateTimeConverter {
 	 */
 	public String getDateTimeValue(String dateTimeStr) {
 		
+		// Ensure the string is in proper ISO form (make adjustments from XML-syntax)
+		if (dateTimeStr.endsWith("Z")) {
+			// No 'T' even though it ends with a 'Z', so add one
+			if (dateTimeStr.indexOf("T") == -1) {
+				StringBuffer buf = new StringBuffer(dateTimeStr);
+				int lastZ = buf.lastIndexOf("Z");
+				buf.insert(lastZ, "T");
+				dateTimeStr = buf.toString();
+			}
+		}
+		Pattern pattern = Pattern.compile(".*[+-]\\d{2}:\\d{2}");
+		Matcher matcher = pattern.matcher(dateTimeStr);
+		if (matcher.matches()) {
+			// There is a timezone, but no 'T', so add one
+			if (dateTimeStr.indexOf("T") == -1) {
+				StringBuffer buf = new StringBuffer(dateTimeStr);
+				int lastColon = buf.lastIndexOf(":");
+				buf.insert(lastColon-3, "T");
+				dateTimeStr = buf.toString();
+			}
+		}		
+				
+		// Create a formatter to parse the input string
 		DateTimeFormatter formatter = ISODateTimeFormat.dateOptionalTimeParser();
 		DateTime dateTime = null;
 		try {
+			// Format the input string as a ISO standard DateTime string
 			dateTime = formatter.parseDateTime(dateTimeStr);
 		} catch (Exception e) {
 			System.out.println("DateTimeConverter ERROR: "+e.getMessage());
