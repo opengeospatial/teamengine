5249d763d785569633071b62668e7e8aaf8db89d
Chuck Morris <chuck.morris@ngc.com>
2008-03-04 20:34:44 +0000

    Fix startup batch files to work when the teamengine apth contains spaces.
    git-svn-id: https://svn.code.sf.net/p/teamengine/code/trunk@288 24c30046-25e5-4eec-8353-25991c8b1332

--- bin/test.bat
+++ bin/test.bat
@@ -1,12 +1,12 @@
 @echo off
 setlocal
 set home=%~dp0
-if exist %home%setenv.bat call %home%setenv.bat
-set cp=%home%..\apps\engine\resources
-for %%a in (%home%..\apps\engine\dist\*.jar) do call :addtocp %%~fa
-for %%a in (%home%..\apps\engine\lib\*.jar) do call :addtocp %%~fa
-for /d %%a in (%home%..\components\*) do call :addcomponent %%a
-for /d %%a in (%home%..\scripts\*) do call :addscript %%a
+if exist "%home%setenv.bat" call "%home%setenv.bat"
+set cp="%home%..\apps\engine\resources"
+for %%a in ("%home%..\apps\engine\dist"\*.jar) do call :addtocp "%%~fa"
+for %%a in ("%home%..\apps\engine\lib"\*.jar) do call :addtocp "%%~fa"
+for /d %%a in ("%home%..\components"\*) do call :addcomponent "%%a"
+for /d %%a in ("%home%..\scripts"\*) do call :addscript "%%a"
 set java="%JAVA_HOME%\bin\java.exe"
 if not exist %java% set java=java
 %java% -cp %cp% %JAVA_OPTS% com.occamlab.te.Test -cmd=%0 %*
@@ -15,7 +15,7 @@ goto:eof
 
 :addcomponent
 set cp=%cp%;%1\resources
-for %%b in (%1\lib\*.jar) do call :addtocp %%~fb
+for %%b in (%1\lib\*.jar) do call :addtocp "%%~fb"
 goto:eof
 
 :addscript
