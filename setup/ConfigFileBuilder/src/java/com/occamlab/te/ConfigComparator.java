a9c1b4a51f419410a647c598113e4d62bb7a53c9
Chuck Morris <chuck.morris@ngc.com>
2008-10-23 20:43:22 +0000

    Add ConfigFileBuilder
    git-svn-id: https://svn.code.sf.net/p/teamengine/code/branches/team2@331 24c30046-25e5-4eec-8353-25991c8b1332

---
+++ setup/ConfigFileBuilder/src/java/com/occamlab/te/ConfigComparator.java
@@ -0,0 +1,26 @@
+package com.occamlab.te;
+
+import java.util.Comparator;
+
+public class ConfigComparator implements Comparator<ConfigEntry> {
+
+    public int compare(ConfigEntry o1, ConfigEntry o2) {
+        int i = o1.organization.compareTo(o2.organization);
+        if (i != 0) return i;
+        i = o1.standard.compareTo(o2.standard);
+        if (i != 0) return i;
+        i = o1.version.compareTo(o2.version);
+        if (i != 0) return i;
+        if (o1.suite != null && o2.suite != null) {
+          return o1.revision.compareTo(o2.revision);
+        }
+        if (o1.suite == null && o2.suite != null) {
+            return -1;
+        }
+        if (o1.suite != null && o2.suite == null) {
+            return 1;
+        }
+        return 0;
+    }
+
+}
---
+++ setup/ConfigFileBuilder/src/java/com/occamlab/te/ConfigEntry.java
@@ -0,0 +1,62 @@
+package com.occamlab.te;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.namespace.QName;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import com.occamlab.te.util.DomUtils;
+
+public class ConfigEntry {
+    public String organization = null;
+    public String standard = null;
+    public String version = null;
+    public String revision = null;
+    public QName suite = null;
+    public String title = null;
+    public String description = null;
+    public List<QName> profiles = new ArrayList<QName>();
+    public List<String> profileTitles = new ArrayList<String>();
+    public List<String> profileDescriptions = new ArrayList<String>();
+    public List<File> sources = new ArrayList<File>();
+    
+    ConfigEntry(File file) throws Exception {
+        DocumentBuilder db = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        Document doc = db.parse(file);
+        Element config = DomUtils.getElementByTagName(doc, "config");
+        organization = DomUtils.getElementByTagName(DomUtils.getElementByTagName(config, "organization"), "name").getTextContent();
+        standard = DomUtils.getElementByTagName(DomUtils.getElementByTagName(config, "standard"), "name").getTextContent();
+        version = DomUtils.getElementByTagName(DomUtils.getElementByTagName(config, "version"), "name").getTextContent();
+        revision = DomUtils.getElementByTagName(DomUtils.getElementByTagName(config, "revision"), "name").getTextContent();
+        Element suiteEl = DomUtils.getElementByTagName(config, "suite");
+        if (suiteEl != null) {
+            String localName = DomUtils.getElementByTagName(suiteEl, "local-name").getTextContent();
+            String namespaceUri = DomUtils.getElementByTagName(suiteEl, "namespace-uri").getTextContent();
+            String prefix = DomUtils.getElementByTagName(suiteEl, "prefix").getTextContent();
+            suite = new QName(namespaceUri, localName, prefix);
+            Element titleEl = DomUtils.getElementByTagName(suiteEl, "title");
+            if (titleEl != null) title = titleEl.getTextContent();
+            Element descriptionEl = DomUtils.getElementByTagName(suiteEl, "description");
+            if (descriptionEl != null) description = descriptionEl.getTextContent();
+        }
+        for (Element profileEl : DomUtils.getElementsByTagName(config, "profile")) {
+            String localName = DomUtils.getElementByTagName(profileEl, "local-name").getTextContent();
+            String namespaceUri = DomUtils.getElementByTagName(profileEl, "namespace-uri").getTextContent();
+            String prefix = DomUtils.getElementByTagName(profileEl, "prefix").getTextContent();
+            profiles.add(new QName(namespaceUri, localName, prefix));
+            Element titleEl = DomUtils.getElementByTagName(suiteEl, "title");
+            if (titleEl != null) profileTitles.add(titleEl.getTextContent());
+            Element descriptionEl = DomUtils.getElementByTagName(suiteEl, "description");
+            if (descriptionEl != null) profileDescriptions.add(descriptionEl.getTextContent());
+        }
+        for (Element sourceEl : DomUtils.getElementsByTagName(config, "source")) {
+            sources.add(new File(file.getParentFile(), sourceEl.getTextContent()));
+        }
+    }
+}
