00dc2b388fc77a67892b5d04ba5bfddbafaae232
Chuck Morris <chuck.morris@ngc.com>
2009-07-07 22:21:08 +0000

    Update ConfigFileBuilder to use a default revision if config files don't contain a revision.
    git-svn-id: https://svn.code.sf.net/p/teamengine/code/branches/team2@399 24c30046-25e5-4eec-8353-25991c8b1332

--- setup/ConfigFileBuilder/src/java/com/occamlab/te/ConfigComparator.java
+++ setup/ConfigFileBuilder/src/java/com/occamlab/te/ConfigComparator.java
@@ -3,6 +3,10 @@ package com.occamlab.te;
 import java.util.Comparator;
 
 public class ConfigComparator implements Comparator<ConfigEntry> {
+    
+    private String denull(String s) {
+        return s == null ? "" : s;
+    }
 
     public int compare(ConfigEntry o1, ConfigEntry o2) {
         int i = o1.organization.compareTo(o2.organization);
@@ -12,7 +16,7 @@ public class ConfigComparator implements Comparator<ConfigEntry> {
         i = o1.version.compareTo(o2.version);
         if (i != 0) return i;
         if (o1.suite != null && o2.suite != null) {
-          return o1.revision.compareTo(o2.revision);
+          return denull(o1.revision).compareTo(denull(o2.revision));
         }
         if (o1.suite == null && o2.suite != null) {
             return -1;
