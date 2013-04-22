c6a6d2c0c23287fa2215541cba48072eb2ed075c
Richard Martell <rmartell@galdosinc.com>
2013-04-22 22:04:01 +0000

    Assemblies built in new teamengine-console module.
    git-svn-id: https://svn.code.sf.net/p/teamengine/code/trunk@821 24c30046-25e5-4eec-8353-25991c8b1332

--- teamengine-core/src/main/scripts/shell/mswin/setenv.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-rem set TE_BASE=%USERPROFILE%\teamengine
-rem mkdir "%TE_BASE%" "%TE_BASE%\work" "%TE_BASE%\users" "%TE_BASE%\scripts" "%TE_BASE%\resources" 2>NUL
-
-rem set JAVA_HOME=C:\PROGRA~1\Java\jdk1.7.0_02
-rem set JAVA_OPTS=-Xmx256m
