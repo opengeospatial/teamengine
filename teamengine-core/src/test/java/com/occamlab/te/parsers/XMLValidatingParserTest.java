589030a4192004ace3332a12906af1f132e2b0a7
Richard Martell <rmartell@galdosinc.com>
2013-09-10 21:42:21 +0000

    Added some unit tests for XMLValidatingParser (note: run slow due to external schema references).
    git-svn-id: https://svn.code.sf.net/p/teamengine/code/trunk@869 24c30046-25e5-4eec-8353-25991c8b1332

---
+++ teamengine-core/src/test/java/com/occamlab/te/parsers/XMLValidatingParserTest.java
@@ -0,0 +1,70 @@
+package com.occamlab.te.parsers;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.net.URL;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.w3c.dom.Document;
+
+public class XMLValidatingParserTest {
+
+    private static DocumentBuilder docBuilder;
+
+    @BeforeClass
+    public static void initFixture() throws ParserConfigurationException {
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        dbf.setNamespaceAware(true);
+        docBuilder = dbf.newDocumentBuilder();
+    }
+
+    @Test
+    public void createParserWithCswSchemaAsResource() throws Exception {
+        Document schemaRefs = docBuilder.parse(getClass().getResourceAsStream(
+                "/schema-refs-csw.xml"));
+        XMLValidatingParser iut = new XMLValidatingParser(schemaRefs);
+        assertNotNull(iut);
+        assertEquals("Unexpected number of schema references", 1,
+                iut.schemaList.size());
+        assertTrue("Expected File instance.",
+                File.class.isInstance(iut.schemaList.get(0)));
+    }
+
+    @Test
+    public void parseCswCapabilities_valid() throws Exception {
+        Document schemaRefs = docBuilder.parse(getClass().getResourceAsStream(
+                "/schema-refs-csw.xml"));
+        URL url = getClass().getResource("/capabilities-pycsw.xml");
+        StringWriter strWriter = new StringWriter();
+        PrintWriter logger = new PrintWriter(strWriter);
+        XMLValidatingParser iut = new XMLValidatingParser();
+        Document result = iut.parse(url.openConnection(),
+                schemaRefs.getDocumentElement(), logger);
+        assertNotNull(result);
+        assertTrue("Unexpected validation error(s) reported.", strWriter
+                .toString().isEmpty());
+    }
+
+    @Test
+    public void parseCswCapabilities_invalid() throws Exception {
+        Document schemaRefs = docBuilder.parse(getClass().getResourceAsStream(
+                "/schema-refs-csw.xml"));
+        URL url = getClass().getResource("/capabilities-invalid.xml");
+        StringWriter strWriter = new StringWriter();
+        PrintWriter logger = new PrintWriter(strWriter);
+        XMLValidatingParser iut = new XMLValidatingParser();
+        Document result = iut.parse(url.openConnection(),
+                schemaRefs.getDocumentElement(), logger);
+        assertNull(result);
+        assertFalse("Expected validation error(s).", strWriter.toString()
+                .isEmpty());
+    }
+
+}
