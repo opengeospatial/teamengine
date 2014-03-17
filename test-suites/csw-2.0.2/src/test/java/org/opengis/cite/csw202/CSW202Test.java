b4bc09a886f7ee8d220cca44c018ce52b798a375
Luis Bermudez <bermud@gmail.com>
2014-03-17 13:25:40 -0400

    Class to test CSW 2.0.2. To be implemented. This is a placeholder example.

--- test-suites/csw-2.0.2/src/test/java/org/opengis/cite/csw202/CSW202Test.java
+++ test-suites/csw-2.0.2/src/test/java/org/opengis/cite/csw202/CSW202Test.java
@@ -10,27 +10,10 @@ import org.junit.Test;
 import org.w3c.dom.Element;
 import com.occamlab.te.index.Index;
 
-public class CSW202TEST {
+public class CSW202Test {
+	
+	public void test() {
+	
+	}
 
-    static Engine engine;
-    static Index index;
-    static RuntimeOptions runOpts;
-
-    @BeforeClass
-    public static void setupFixture() throws Exception {
-        engine = new Engine();
-        index = new Index();
-        runOpts = new RuntimeOptions();
-    }
-
-    @Test
-    public void parseXmlWithNullInstruction() throws Throwable {
-        URL url = this.getClass().getResource("/article.xml");
-        URLConnection urlConn = url.openConnection();
-        TECore iut = new TECore(engine, index, runOpts);
-        Element result = iut.parse(urlConn, null);
-        Element content = (Element) result.getElementsByTagName("content")
-                .item(0);
-        assertNotNull("Expected content element.", content);
-        assertTrue("Expected XML content ", content.hasChildNodes());
-    }
+}
