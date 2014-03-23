92781f2089535f34157954b51aa3c9772cc04141
R. Martell <rjmartell@computer.org>
2014-03-22 19:46:07 -0700

    Unpack and remove archives exported into TE_BASE/scripts.

--- teamengine-console/src/main/scripts/shell/mswin/export-ctl.bat
+++ teamengine-console/src/main/scripts/shell/mswin/export-ctl.bat
@@ -1,16 +1,22 @@
 :: Reads a CSV file (first argument) where each record contains two fields:
 :: Subversion tag URL, local path name relative to TE_BASE/scripts
 :: Example:
-:: http://svn.example.org/scripts/alpha/1.0.0/trunk,alpha/1.0.0
+:: http://svn.example.org/scripts/alpha/1.0.0/tags/r2,alpha/1.0.0
 
 @echo off
 setlocal
 
 set home=%~dp0
 if exist "%home%setenv.bat" call "%home%setenv.bat"
+if "%JAVA_HOME%"=="" echo JAVA_HOME must be set. & goto end
 
 set csvfile=%~f1
 
 for /F "usebackq tokens=1,2 delims=," %%a in ("%csvfile%") do (
   svn -q export %%a %TE_BASE%\scripts\%%b
 )
+
+cd /d %TE_BASE%\scripts
+for %%f in (*.zip) do ("%JAVA_HOME%"\bin\jar xf %%f & del %%f)
+
+:end
--- teamengine-console/src/main/scripts/shell/unix/export-ctl.sh
+++ teamengine-console/src/main/scripts/shell/unix/export-ctl.sh
@@ -1,13 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 # Reads a CSV file (first argument) where each record contains two fields:
 # Subversion tag URL, local path name relative to TE_BASE/scripts
 # Example:
-# http://svn.example.org/scripts/alpha/1.0.0/trunk,alpha/1.0.0
+# http://svn.example.org/scripts/alpha/1.0.0/tags/r2,alpha/1.0.0
+
 base=`dirname $0`
 if [ -r $base/setenv.sh ]
 then
   . $base/setenv.sh
 fi
+[ -z "$JAVA_HOME" ] && echo "JAVA_HOME must be set." && exit 1
 
 csvfile="$1"
 
@@ -15,3 +17,13 @@ while IFS="," read url etspath
 do
   svn -q export $url $TE_BASE/scripts/$etspath
 done < "$csvfile"
+
+pushd $TE_BASE/scripts
+# filename patterns which match no files will expand to null string
+shopt -s nullglob
+for f in *.zip
+do
+  "$JAVA_HOME"/bin/jar xf "$f"
+  rm "$f"
+done
+
