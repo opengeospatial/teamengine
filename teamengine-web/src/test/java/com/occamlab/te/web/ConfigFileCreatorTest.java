d43766cd2aa05b27bdd8fc91b4827207a6e429c6
R. Martell <rjmartell@computer.org>
2015-09-24 13:25:27 -0700

    Format unit test class

--- teamengine-web/src/test/java/com/occamlab/te/web/ConfigFileCreatorTest.java
+++ teamengine-web/src/test/java/com/occamlab/te/web/ConfigFileCreatorTest.java
@@ -1,9 +1,12 @@
 package com.occamlab.te.web;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -17,215 +20,172 @@ import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 public class ConfigFileCreatorTest {
-	private File classp;
-	private DocumentBuilder builder;
+    private File classp;
+    private DocumentBuilder builder;
 
-	@Before
-	public void getcp() {
+    @Before
+    public void createDocBuilder() throws ParserConfigurationException, URISyntaxException {
+        this.classp = new File(getClass().getResource("/").toURI());
+        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+        builder = documentBuilderFactory.newDocumentBuilder();
+    }
 
-		classp = new File(ConfigFileCreatorTest.class.getProtectionDomain()
-				.getCodeSource().getLocation().getPath());
+    @Test
+    public void testMultipleTestsUnderTEBASE() throws URISyntaxException {
+        File tebase = new File(classp, "tebase");
+        ConfigFileCreator configFileCreator = new ConfigFileCreator();
+        configFileCreator.create(tebase);
+        File configFile = new File(tebase, "config.xml");
+        try {
+            Document config = builder.parse(configFile);
+            Node node = XMLUtils.getFirstNode(config,
+                    "/config/scripts/organization[child::name/text()='OGC']/standard[child::name/text()='Catalogue Service - Web (CSW)']/"
+                            + "version[child::name/text()='3.0.0']/suite/namespace-uri");
+            assertTrue("The config file of a test (e.g. CAT 3.0) was copied deepely",
+                    node.getTextContent().equals("http://www.opengis.net/cite/cat30"));
+            NodeList orgs = XMLUtils.getAllNodes(config, "/config/scripts/organization");
+            assertEquals("Expected 1 organization", 1, orgs.getLength());
+            String xpath = "/config/scripts/organization[child::name/text()='OGC']/standard[child::name/text()='Catalogue Service - Web (CSW)']/version";
+            NodeList vers = XMLUtils.getAllNodes(config, xpath);
+            assertEquals("Expected 2 CSW standards", 2, vers.getLength());
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-		// System.out.println(classp.getPath()+" exists "+classp.exists());
-		createBuilder();
+    }
 
-	}
+    @Test
+    public void testParsingMultipleOranizations() {
 
-	private void createBuilder() {
-		DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory
-				.newInstance();
-		try {
-			builder = documentBuilderFactory.newDocumentBuilder();
-		} catch (ParserConfigurationException e) {
+        try {
+            File tebase = new File(classp, "tebase-multiple-organizations");
+            ConfigFileCreator configFileCreator = new ConfigFileCreator();
+            configFileCreator.create(tebase);
+            File configFile = new File(tebase + File.separator + "config.xml");
 
-			e.printStackTrace();
-		}
-	}
+            Document config = builder.parse(configFile);
 
-	@Test
-	public void testMultipleTestsUnderTEBASE() {
-		File tebase = new File(classp, "tebase");
+            NodeList orgs = XMLUtils.getAllNodes(config, "/config/scripts/organization");
+            assertEquals(2, orgs.getLength());
 
-		ConfigFileCreator configFileCreator = new ConfigFileCreator();
-		configFileCreator.create(tebase);
-		File configFile = new File(tebase + File.separator + "config.xml");
-		try {
-			Document config = builder.parse(configFile);
-			Node node = XMLUtils
-					.getFirstNode(
-							config,
-							"/config/scripts/organization[child::name/text()='OGC']/standard[child::name/text()='Catalogue Service - Web (CSW)']/"
-									+ "version[child::name/text()='3.0.0']/suite/namespace-uri");
-			assertTrue(
-					"The config file of a test (e.g. CAT 3.0) was copied deepely",
-					node.getTextContent().equals(
-							"http://www.opengis.net/cite/cat30"));
+            configFile.delete();
 
-			NodeList orgs = XMLUtils.getAllNodes(config,
-					"/config/scripts/organization");
-			assertEquals("Only 2 organizations where created", 2,
-					orgs.getLength());
+        } catch (Exception e) {
+            fail();
+            e.printStackTrace();
+        }
 
-			String xpath = "/config/scripts/organization[child::name/text()='OGC']/standard[child::name/text()='Catalogue Service - Web (CSW)']/version";
-			System.out.println(xpath);
-			NodeList vers = XMLUtils.getAllNodes(config, xpath);
-			assertEquals("Only 2 standards where created under CSW", 2,
-					vers.getLength());
-			configFile.delete();
+    }
 
-		} catch (SAXException e) {
+    @Test
+    public void testParsingFoldersWithHiddenFiles() {
 
-			e.printStackTrace();
-		} catch (IOException e) {
+        try {
+            File tebase = new File(classp, "tebase-hidden-files");
+            ConfigFileCreator configFileCreator = new ConfigFileCreator();
+            configFileCreator.create(tebase);
+            File configFile = new File(tebase + File.separator + "config.xml");
 
-			e.printStackTrace();
-		}
+            Document config = builder.parse(configFile);
 
-	}
+            NodeList orgs = XMLUtils.getAllNodes(config, "/config/scripts/organization");
+            assertEquals(2, orgs.getLength());
 
-	@Test
-	public void testParsingMultipleOranizations() {
+            configFile.delete();
 
-		try {
-			File tebase = new File(classp, "tebase-multiple-organizations");
-			ConfigFileCreator configFileCreator = new ConfigFileCreator();
-			configFileCreator.create(tebase);
-			File configFile = new File(tebase + File.separator + "config.xml");
+        } catch (Exception e) {
+            fail();
+            e.printStackTrace();
+        }
+    }
 
-			Document config = builder.parse(configFile);
+    @Test
+    public void testSingleTestTEBASEwithNoConfigFileInATest() {
 
-			NodeList orgs = XMLUtils.getAllNodes(config,
-					"/config/scripts/organization");
-			assertEquals(2, orgs.getLength());
+        try {
+            File tebase = new File(classp, "tebase-no-config-in-test");
+            ConfigFileCreator configFileCreator = new ConfigFileCreator();
+            configFileCreator.create(tebase);
+            File configFile = new File(tebase + File.separator + "config.xml");
 
-			configFile.delete();
+            Document config = builder.parse(configFile);
 
-		} catch (Exception e) {
-			fail();
-			e.printStackTrace();
-		}
+            NodeList orgs = XMLUtils.getAllNodes(config, "/config/scripts/organization");
+            assertEquals(0, orgs.getLength());
 
-	}
-	
-	@Test
-	public void testParsingFoldersWithHiddenFiles() {
+            configFile.delete();
 
-		try {
-			File tebase = new File(classp, "tebase-hidden-files");
-			ConfigFileCreator configFileCreator = new ConfigFileCreator();
-			configFileCreator.create(tebase);
-			File configFile = new File(tebase + File.separator + "config.xml");
+        } catch (Exception e) {
+            fail();
+            e.printStackTrace();
+        }
 
-			Document config = builder.parse(configFile);
+    }
 
-			NodeList orgs = XMLUtils.getAllNodes(config,
-					"/config/scripts/organization");
-			assertEquals(2, orgs.getLength());
+    @Test
+    public void testSingleTestTEBASEwithScriptsEmpty() {
 
-			configFile.delete();
+        try {
+            File tebase = new File(classp, "tebase-empty");
+            ConfigFileCreator configFileCreator = new ConfigFileCreator();
+            configFileCreator.create(tebase);
+            File configFile = new File(tebase + File.separator + "config.xml");
 
-		} catch (Exception e) {
-			fail();
-			e.printStackTrace();
-		}
+            Document config = builder.parse(configFile);
 
-	}
-	
-	@Test
-	public void testSingleTestTEBASEwithNoConfigFileInATest() {
+            NodeList orgs = XMLUtils.getAllNodes(config, "/config/scripts/organization");
+            assertEquals(0, orgs.getLength());
 
-		try {
-			File tebase = new File(classp, "tebase-no-config-in-test");
-			ConfigFileCreator configFileCreator = new ConfigFileCreator();
-			configFileCreator.create(tebase);
-			File configFile = new File(tebase + File.separator + "config.xml");
+            configFile.delete();
 
-			Document config = builder.parse(configFile);
+        } catch (Exception e) {
+            fail();
+            e.printStackTrace();
+        }
 
-			NodeList orgs = XMLUtils.getAllNodes(config,
-					"/config/scripts/organization");
-			assertEquals(0, orgs.getLength());
+    }
 
-			configFile.delete();
+    @Test
+    public void testTEBASEnotfound() {
 
-		} catch (Exception e) {
-			fail();
-			e.printStackTrace();
-		}
+        File tebase = new File(classp, "xyzzxsyss");
+        ConfigFileCreator configFileCreator = new ConfigFileCreator();
+        try {
+            configFileCreator.create(tebase);
+        } catch (TEException e) {
+            assertEquals("com.occamlab.te.web.TEBaseNotFoundException", e.getClass().getName());
+            return;
 
-	}
-	
-	@Test
-	public void testSingleTestTEBASEwithScriptsEmpty() {
+        }
+        fail("it should throw aTEBaseNotFoundException Exception");
+    }
 
-		try {
-			File tebase = new File(classp, "tebase-empty");
-			ConfigFileCreator configFileCreator = new ConfigFileCreator();
-			configFileCreator.create(tebase);
-			File configFile = new File(tebase + File.separator + "config.xml");
+    /**
+     * Tests to properly parsed when multiple versions of a test exist. for
+     * example SFS 1.2,1.2,1.3 etc, and they are all under SFS directory.
+     */
+    @Test
+    public void testMultipleVersionsOfATest() {
+        try {
 
-			Document config = builder.parse(configFile);
+            File tebase = new File(classp, "tebase-multiple-versions-of-a-test");
+            ConfigFileCreator configFileCreator = new ConfigFileCreator();
+            configFileCreator.create(tebase);
+            File configFile = new File(tebase + File.separator + "config.xml");
+            Document config = builder.parse(configFile);
+            NodeList numberOfTests = XMLUtils.getAllNodes(config, "/config/scripts/organization/standard/version");
+            assertEquals(3, numberOfTests.getLength());
 
-			NodeList orgs = XMLUtils.getAllNodes(config,
-					"/config/scripts/organization");
-			assertEquals(0, orgs.getLength());
+        } catch (SAXException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
 
-			configFile.delete();
-
-		} catch (Exception e) {
-			fail();
-			e.printStackTrace();
-		}
-
-	}
-	
-	@Test
-	public void testTEBASEnotfound() {
-
-
-			File tebase = new File(classp, "xyzzxsyss");
-			ConfigFileCreator configFileCreator = new ConfigFileCreator();
-			try{
-				configFileCreator.create(tebase);
-			}catch (TEException e){
-				assertEquals("com.occamlab.te.web.TEBaseNotFoundException", e.getClass().getName());
-				return;
-				
-			}
-			fail("it should throw aTEBaseNotFoundException Exception");
-	}
-	
-	/**
-	 * Tests to properly parsed when multiple versions of a test exist. for example SFS 1.2,1.2,1.3 etc, 
-	 * and they are all under SFS directory.
-	 */
-	@Test
-	public void testMultipleVersionsOfATest(){
-		try {
-		
-		File tebase = new File(classp, "tebase-multiple-versions-of-a-test");
-		ConfigFileCreator configFileCreator = new ConfigFileCreator();
-		configFileCreator.create(tebase);
-		File configFile = new File(tebase + File.separator + "config.xml");
-		Document config = builder.parse(configFile);
-		NodeList numberOfTests = XMLUtils.getAllNodes(config,
-				"/config/scripts/organization/standard/version");
-		assertEquals(3, numberOfTests.getLength());
-		
-			
-		} catch (SAXException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		
-		
-		
-	}
-	
-
+    }
 
 }
