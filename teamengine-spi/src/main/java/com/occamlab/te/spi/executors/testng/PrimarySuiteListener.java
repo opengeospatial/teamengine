7ec1358bf810ed6449bbe6c1ae1c72e3ef17fcd6
R. Martell <rjmartell@computer.org>
2015-10-30 11:13:55 -0700

    Add logging messages (FINE)

--- teamengine-spi/src/main/java/com/occamlab/te/spi/executors/testng/PrimarySuiteListener.java
+++ teamengine-spi/src/main/java/com/occamlab/te/spi/executors/testng/PrimarySuiteListener.java
@@ -1,6 +1,9 @@
 package com.occamlab.te.spi.executors.testng;
 
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.testng.ISuite;
 import org.testng.ISuiteListener;
 import org.w3c.dom.Document;
@@ -15,6 +18,7 @@ import org.w3c.dom.NodeList;
  */
 class PrimarySuiteListener implements ISuiteListener {
 
+    private final static Logger LOGR = Logger.getLogger(PrimarySuiteListener.class.getName());
     private Document testRunArgs;
 
     /**
@@ -28,19 +32,30 @@ class PrimarySuiteListener implements ISuiteListener {
      * @see java.util.Properties#loadFromXML(java.io.InputStream) loadFromXML
      */
     PrimarySuiteListener(Document testRunArgs) {
+        LOGR.log(Level.FINE, "Initializing PrimarySuiteListener...");
+        if (null == testRunArgs) {
+            throw new NullPointerException("Test run input is null");
+        }
+        NodeList entries = testRunArgs.getElementsByTagName("entry");
+        if (entries.getLength() == 0) {
+            throw new IllegalArgumentException(
+                    String.format("No test run arguments found in %s", testRunArgs.getDocumentElement().getNodeName()));
+        }
         this.testRunArgs = testRunArgs;
     }
 
     @Override
     public void onStart(ISuite suite) {
-        if (null == this.testRunArgs) {
-            return;
-        }
         Map<String, String> params = suite.getXmlSuite().getParameters();
         NodeList entries = testRunArgs.getElementsByTagName("entry");
         for (int i = 0; i < entries.getLength(); i++) {
             Element entry = (Element) entries.item(i);
-            params.put(entry.getAttribute("key"), entry.getTextContent());
+            String value = entry.getTextContent().trim();
+            if (value.isEmpty()) {
+                continue;
+            }
+            params.put(entry.getAttribute("key"), value);
+            LOGR.log(Level.FINE, "Added parameter: {0}={1}", new Object[] { entry.getAttribute("key"), value });
         }
     }
 
