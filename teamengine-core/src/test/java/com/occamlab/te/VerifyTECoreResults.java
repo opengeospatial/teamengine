5540c74035cf32ceac5dbb6d927012ce277a39ea
R. Martell <rjmartell@computer.org>
2016-01-26 14:11:37 -0800

    Add unit tests for issue #141

---
+++ teamengine-core/src/test/java/com/occamlab/te/VerifyTECoreResults.java
@@ -0,0 +1,96 @@
+package com.occamlab.te;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.util.Collections;
+
+import org.junit.After;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.occamlab.te.index.Index;
+import com.occamlab.te.index.SuiteEntry;
+import com.occamlab.te.index.TestEntry;
+
+public class VerifyTECoreResults {
+
+	static Engine engine;
+	static RuntimeOptions runOpts;
+
+	@BeforeClass
+	public static void setupFixture() throws Exception {
+		engine = new Engine();
+		runOpts = new RuntimeOptions();
+		runOpts.setLogDir(null);
+	}
+
+	@After
+	public void clearParams() {
+		runOpts.getParams().clear();
+	}
+
+	@Test
+	public void allSubtestsSkipped() throws Throwable {
+		Index testIndex = getTestIndex(new File(
+				"src/test/resources/ctl/skip.ctl"));
+		engine.addFunctionLibrary(Collections.singletonList(testIndex));
+		runOpts.addParam("input=103");
+		TECore teCore = new TECore(engine, testIndex, runOpts);
+		assertNotNull(teCore);
+		teCore.execute();
+		SuiteEntry testSuite = teCore.getIndex().getSuite("ex:skip");
+		assertNotNull(testSuite);
+		TestEntry mainTest = teCore.getIndex().getTest(
+				testSuite.getStartingTest());
+		assertNotNull(mainTest);
+		// Main test result should be SKIP
+		int mainResult = mainTest.getResult();
+		assertEquals(
+				String.format("Unexpected main result: '%s'.",
+						TECore.getResultDescription(mainResult)),
+				TECore.SKIPPED, mainResult);
+		assertTestResult(teCore.getIndex(), "test-1", TECore.SKIPPED);
+		assertTestResult(teCore.getIndex(), "test-2", TECore.SKIPPED);
+	}
+
+	@Test
+	public void someSubtestsSkipped() throws Throwable {
+		Index testIndex = getTestIndex(new File(
+				"src/test/resources/ctl/skip.ctl"));
+		engine.addFunctionLibrary(Collections.singletonList(testIndex));
+		runOpts.addParam("input=41");
+		TECore teCore = new TECore(engine, testIndex, runOpts);
+		assertNotNull(teCore);
+		teCore.execute();
+		SuiteEntry testSuite = teCore.getIndex().getSuite("ex:skip");
+		assertNotNull(testSuite);
+		TestEntry mainTest = teCore.getIndex().getTest(
+				testSuite.getStartingTest());
+		assertNotNull(mainTest);
+		// Main test result should be PASS
+		int mainResult = mainTest.getResult();
+		assertEquals(
+				String.format("Unexpected main result: '%s'.",
+						TECore.getResultDescription(mainResult)), TECore.PASS,
+				mainResult);
+		assertTestResult(teCore.getIndex(), "test-1", TECore.SKIPPED);
+		assertTestResult(teCore.getIndex(), "test-2", TECore.PASS);
+	}
+
+	Index getTestIndex(File ctlFile) throws Throwable {
+		assertTrue(ctlFile.exists());
+		SetupOptions setupOptions = new SetupOptions();
+		setupOptions.addSource(ctlFile);
+		Index testIndex = Generator.generateXsl(setupOptions);
+		assertNotNull(testIndex);
+		return testIndex;
+	}
+
+	void assertTestResult(Index index, String testName, int expectedResult)
+			throws Throwable {
+		TestEntry test = index.getTest(testName);
+		assertNotNull(test);
+		assertEquals(expectedResult, test.getResult());
+	}
+}
---
+++ teamengine-core/src/test/resources/ctl/skip.ctl
@@ -0,0 +1,56 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<ctl:package xmlns:ctl="http://www.occamlab.com/ctl"
+  xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+  xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+  xmlns:ex="http://example.org/">
+
+  <ctl:suite name="ex:skip">
+    <ctl:title>Sample test suite</ctl:title>
+    <ctl:description>Skip all constituent tests (odd integer greater than 100).</ctl:description>
+    <ctl:starting-test>ex:skip-main</ctl:starting-test>
+  </ctl:suite>
+
+  <ctl:test name="ex:skip-main">
+    <ctl:param name="input"/>
+    <ctl:assertion>The input number satisfies all mandatory constraints.</ctl:assertion>
+    <ctl:code>
+      <xsl:choose>
+        <xsl:when test="$input castable as xsd:integer">
+          <ctl:call-test name="ex:test-1">
+            <ctl:with-param name="int" select="$input"/>
+          </ctl:call-test>
+          <ctl:call-test name="ex:test-2">
+            <ctl:with-param name="int" select="$input"/>
+          </ctl:call-test>
+        </xsl:when>
+        <xsl:otherwise>
+          <ctl:message>Non-integer input: <xsl:value-of select="$input"/></ctl:message>
+          <ctl:fail />
+        </xsl:otherwise>
+      </xsl:choose>
+    </ctl:code>
+  </ctl:test>
+
+  <ctl:test name="ex:test-1">
+    <ctl:param name="int"/>
+    <ctl:assertion>Accept even integers.</ctl:assertion>
+    <ctl:code>
+      <xsl:if test="(xsd:integer($int) mod 2) != 0">
+        <ctl:message>[SKIP] Not an even number: <xsl:value-of select="$int"/></ctl:message>
+        <ctl:skipped />
+      </xsl:if>
+    </ctl:code>
+  </ctl:test>
+
+  <ctl:test name="ex:test-2">
+    <ctl:param name="int"/>
+    <ctl:assertion>Accept integers less than 100.</ctl:assertion>
+    <ctl:code>
+      <xsl:if test="xsd:integer($int) ge 100">
+        <ctl:message>[SKIP] <xsl:value-of select="$int"/> is greater than 100.</ctl:message>
+        <ctl:skipped/>
+      </xsl:if>
+    </ctl:code>
+  </ctl:test>
+
+</ctl:package>
