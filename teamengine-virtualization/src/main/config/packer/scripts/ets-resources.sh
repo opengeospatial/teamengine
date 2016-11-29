9957a6705383b17d85a3154f230fa52943a181fe
R. Martell <rjmartell@computer.org>
2016-11-29 15:05:11 -0800

    Remove redundant CSV file

--- teamengine-virtualization/src/main/config/packer/scripts/ets-resources.sh
+++ teamengine-virtualization/src/main/config/packer/scripts/ets-resources.sh
@@ -6,11 +6,6 @@
 # Note: Maven and Git must be installed and available on the system path.
 
 cd $HOME
-# Fix line endings (strip CR chars)
-if [ -f ets-releases.csv ]; then
-  sed -i 's/\r//' ets-releases.csv
-fi
-
 # Modify ~/.bash_profile in place (login shell)
 sed -i '/^PATH=/ i export TE_BASE=/srv/teamengine' .bash_profile
 sed -i '/^PATH=/ i export ETS_SRC=/usr/local/src/cite' .bash_profile
@@ -29,4 +24,4 @@ mvn clean package
 cd $HOME; mkdir ets-resources
 tar xzf $ETS_SRC/ets-resources/target/ets-resources-*.tar.gz -C ets-resources
 cd ets-resources
-./bin/unix/setup-tebase.sh $HOME/ets-releases.csv
+./bin/unix/setup-tebase.sh ./ets-releases.csv
