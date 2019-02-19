8816d5978ffafb8f1e6c1ac754142da720562903
Keshav Nangare <keshav.nangare@mindstix.com>
2019-02-19 15:46:27 +0530

    Teamengine is upgrade to Jax-rs 2.1.5 and Jersey 2.28.

--- teamengine-spi/src/main/java/com/occamlab/te/spi/jaxrs/ApplicationComponents.java
+++ teamengine-spi/src/main/java/com/occamlab/te/spi/jaxrs/ApplicationComponents.java
@@ -1,16 +1,18 @@
 package com.occamlab.te.spi.jaxrs;
 
-import com.sun.jersey.api.core.PackagesResourceConfig;
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
+import org.glassfish.jersey.server.ResourceConfig;
 
 /**
  * Dynamically searches for root resource and provider classes. An instance of
  * this class defines the components of a JAX-RS application and is used to
  * configure a runtime environment such as a web container.
  * 
- * @see com.sun.jersey.api.core.PackagesResourceConfig
+ * @see org.glassfish.jersey.server.ResourceConfig
  * @see javax.ws.rs.core.Application
  */
-public class ApplicationComponents extends PackagesResourceConfig {
+
+public class ApplicationComponents extends ResourceConfig {
 
     /**
      * Default constructor scans for root resource classes in the
@@ -18,7 +20,9 @@ public class ApplicationComponents extends PackagesResourceConfig {
      * sub-package).
      */
     public ApplicationComponents() {
-        super("com.occamlab.te.spi.jaxrs.resources");
+   // Register resources and providers using package-scanning.
+      packages(true, "com.occamlab.te.spi.jaxrs.resources");
+      register(MultiPartFeature.class);
     }
 
 }
