3adaeb6d049127e8ffbbe5d8c085effbf8ffc333
Dirk Stenger <stenger@lat-lon.de>
2024-02-28 16:00:25 +0100

    #403 - Add license headers

--- teamengine-resources/src/main/resources/com/occamlab/te/earl/lib/js/shBrushAS3.js
+++ teamengine-resources/src/main/resources/com/occamlab/te/earl/lib/js/shBrushAS3.js
@@ -24,6 +24,26 @@
 		// Created by Peter Atoria @ http://iAtoria.com
 	
 		var inits 	 =  'class interface function package';
+
+/*-
+ * #%L
+ * TEAM Engine - Shared Resources
+ * %%
+ * Copyright (C) 2006 - 2024 Open Geospatial Consortium
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 	
 		var keywords =	'-Infinity ...rest Array as AS3 Boolean break case catch const continue Date decodeURI ' + 
 						'decodeURIComponent default delete do dynamic each else encodeURI encodeURIComponent escape ' + 
--- teamengine-resources/src/main/resources/com/occamlab/te/earl/lib/js/shBrushPerl.js
+++ teamengine-resources/src/main/resources/com/occamlab/te/earl/lib/js/shBrushPerl.js
@@ -48,6 +48,26 @@
 			'bless caller continue dbmclose dbmopen die do dump else elsif eval exit ' +
 			'for foreach goto if import last local my next no our package redo ref ' + 
 			'require return sub tie tied unless untie until use wantarray while';
+
+/*-
+ * #%L
+ * TEAM Engine - Shared Resources
+ * %%
+ * Copyright (C) 2006 - 2024 Open Geospatial Consortium
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
     
 		this.regexList = [
 			{ regex: new RegExp('#[^!].*$', 'gm'),					css: 'comments' },
